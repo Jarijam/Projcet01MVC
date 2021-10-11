430e85f641915bda1a002e8cbcef6e05fbb82289
Jaehyun Han <wogusgks@gmail.com>
2021-10-12 01:08:14 +0900

    add controller

--- src/main/java/multi/project/userinfo/UserController.java
+++ src/main/java/multi/project/userinfo/UserController.java
@@ -1,8 +1,17 @@
 package multi.project.userinfo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
+	@Autowired
+	UserService service;
 	
+	@RequestMapping("/user/insert.do")
+	public String insert(UserVO command) {
+		service.insert(command);
+		return "redirect:/index.do";
+	}
 }
