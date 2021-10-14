c4e44dc9d161b7c77425fec1a425afb507226bd2
JaehyunHan0722 <88475359+JaehyunHan0722@users.noreply.github.com>
2021-10-14 18:48:16 +0900

    small change

--- src/main/java/multi/project/userinfo/UserController.java
+++ src/main/java/multi/project/userinfo/UserController.java
@@ -9,7 +9,7 @@ public class UserController {
 	@Autowired
 	UserService service;
 	
-	@RequestMapping("/user/insert.do")
+	@RequestMapping("/insert.do")
 	public String insert(UserVO command) {
 		System.out.println("인서트 매핑!");
 //		service.insert(command);
