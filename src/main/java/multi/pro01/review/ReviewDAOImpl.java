bc522c67a279c736bd7f0063952148add4af5ebc
JaehyunHan0722 <88475359+JaehyunHan0722@users.noreply.github.com>
2021-10-15 15:42:51 +0900

    change

---
+++ src/main/java/multi/pro01/review/ReviewDAOImpl.java
@@ -0,0 +1,25 @@
+package multi.project.review;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReviewDAOImpl implements ReviewDAO {
+
+	@Autowired
+	SqlSession sqlSession;
+	@Override
+	public int insert(ReviewVO reiview) {
+		return 0;
+	}
+	
+	@Override
+	public List<ReviewVO> reviewList() {
+		return null;
+	}
+
+	
+}
