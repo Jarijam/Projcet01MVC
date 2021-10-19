d51a1577aa6f6fc68468f27e6cb39ad66c1a1140
JaehyunHan0722 <88475359+JaehyunHan0722@users.noreply.github.com>
2021-10-19 18:10:01 +0900

    login

--- src/main/java/multi/pro01/restaurant/RestaurantService.java
+++
@@ -1,8 +0,0 @@
-package multi.pro01.restaurant;
-
-import java.util.List;
-
-public interface RestaurantService {
-	List<RestaurantVO> restaurantList();	
-	int insert(RestaurantVO restaurant_name);
-}
