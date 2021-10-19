9855a17d976e9ef9eb09f59de146e53396ef413c
Jeongwon Lee <85335101+Jarijam@users.noreply.github.com>
2021-10-19 17:58:51 +0900

    user update

--- src/main/java/multi/pro01/userinfo/UserController.java
+++ src/main/java/multi/pro01/userinfo/UserController.java
@@ -1,10 +1,13 @@
 package multi.pro01.userinfo;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -31,5 +34,13 @@ public class UserController {
 			}
 		}
 		return result;
+	}
+	@RequestMapping("/user/list.do")
+	public ModelAndView getMemberList() {
+		ModelAndView mav = new ModelAndView();
+		List<UserVO> userlist = service.getMemberList();
+		
+		mav.setViewName(viewName);
+		return mav;
 	}
 }
